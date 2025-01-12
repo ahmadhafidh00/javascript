326bf0d00c9ddc63ddb98827ec220aec0a838e11
ahmadhafidh00 <hafidhahmad04@gmail.com>
2025-01-12 16:39:19 +0700

    Remove directory random-number-generator

--- random-number-generator/1.js
+++
@@ -1,6 +0,0 @@
-function getRandom(max) {
-  return Math.floor(Math.random() * max);
-}
-
-console.log(getRandom(1)); // 0
-console.log(getRandom(6)); // 0 - 5
--- random-number-generator/2.js
+++
@@ -1,6 +0,0 @@
-function getRandom(max) {
-  return Math.floor(Math.random() * max) + 1;
-}
-
-console.log(getRandom(2)); // 1 - 2
-console.log(getRandom(9)); // 1 - 9
